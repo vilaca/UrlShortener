7e6a78d009b10bfdfae6d62e0bec302a36bae311
João Vilaça <jvilaca@gmail.com>
2013-08-03 10:43:20 +0100

    FIX: removed client side URL validation

--- src/main/resources/ajax.js
+++ src/main/resources/ajax.js
@@ -13,23 +13,10 @@ function getUrlFromForm() {
     return url;
 }
 
-//Based on
-//http://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-an-url
-//Changes: made comma valid in path, querystring or fragment
 function isValidUrl(url) {
 
-    if (url.indexOf('http://go2.pt') === 0) {
-        return false;
-    }
+	return url.indexOf('http://go2.pt') !== 0
 
-    var pattern = new RegExp('^(https?:\/\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\/[-a-z,\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z,\\d%_.~+=-]*)?' + // query string
-    '(\\#[-a-z,\\d_]*)?$', 'i'); // fragment locater
-
-    return pattern.test(url);
 }
 
 window.onload = function () {
