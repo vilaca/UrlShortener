2b01d1bd75c4d8d2b5f4f6cad15adc29472a0951
João Vilaça <jvilaca@gmail.com>
2013-09-29 01:18:54 +0100

    ADD: RedirectPageLet for 302/301 redirects, instead of StaticPageLet

---
+++ src/main/java/pt/go2/pagelets/RedirectPageLet.java
@@ -0,0 +1,29 @@
+package pt.go2.pagelets;
+
+import java.io.IOException;
+
+import pt.go2.services.HttpResponse;
+
+import com.sun.net.httpserver.HttpExchange;
+
+/**
+ * @author vilaca
+ * 
+ */
+public class RedirectPageLet implements PageLet {
+
+	private final HttpResponse response;
+	private final String redirect;
+
+	public RedirectPageLet(int responseCode, String redirect) {
+		this.response = HttpResponse.create("text/html", "".getBytes(),
+				responseCode);
+		this.redirect = redirect;
+	}
+
+	@Override
+	public HttpResponse getPageLet(HttpExchange exchange) throws IOException {
+		exchange.getResponseHeaders().set("Location", redirect);
+		return response;
+	}
+}
