66ef8a4fb0d843f71025f28cae3f1e43bb7511c5
João Vilaça <jvilaca@gmail.com>
2013-10-22 01:54:46 +0100

    ADD: Uri test

---
+++ src/test/java/pt/go2/keystore/UriTest.java
@@ -0,0 +1,97 @@
+
+package pt.go2.keystore;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+
+/**
+ * Test Uri class, don't mind testing URL validator
+ * 
+ * @author vilaca
+ *
+ */
+public class UriTest {
+
+	@Test
+	public void testEqual() {
+		
+		final Uri u1 = Uri.create("http://abcdefghij.ws/123");
+		final Uri u2 = Uri.create("http://ABCDEFGHIJ.ws/123");
+		final Uri u3 = Uri.create("http://ABCDEFGHIJ.WS/123");
+
+		Assert.assertNotNull(u1);
+		Assert.assertNotNull(u2);
+		Assert.assertNotNull(u3);		
+		
+		Assert.assertThat(u1, is(u2));
+		Assert.assertThat(u2, is(u3));
+		Assert.assertThat(u3, is(u1));
+	}
+
+
+	@Test
+	public void testEqualHttp() {
+		
+		final Uri u1 = Uri.create("http://abcdefghij.ws/123");
+		final Uri u2 = Uri.create("ABCDEFGHIJ.ws/123");
+
+		Assert.assertNotNull(u1);
+		Assert.assertNotNull(u2);
+		
+		Assert.assertThat(u1, is(u2));
+	}
+
+	@Test
+	public void testTrim() {
+		
+		final Uri u1 = Uri.create(" http://abcdefghij.ws/123");
+		final Uri u2 = Uri.create("http://abcdefghij.ws/123 ");
+		final Uri u3 = Uri.create(" http://abcdefghij.ws/123 ");
+		
+		Assert.assertNotNull(u1);
+		Assert.assertNotNull(u2);
+		Assert.assertNotNull(u3);
+		
+		Assert.assertThat(u1, is(u2));
+		Assert.assertThat(u2, is(u3));
+		Assert.assertThat(u3, is(u1));
+	}
+
+	@Test
+	public void testDifferentScheme() {
+		
+		final Uri u1 = Uri.create("http://abcdefghij.ws/123");
+		final Uri u2 = Uri.create("https://ABCDEFGHIJ.ws/123");
+		
+		Assert.assertNotNull(u1);
+		Assert.assertNotNull(u2);
+		
+		Assert.assertThat(u1, is(not(u2)));
+	}
+
+	@Test
+	public void testDifferentCaseInPath() {
+		
+		final Uri u1 = Uri.create("http://abcdefghij.ws/123A");
+		final Uri u2 = Uri.create("http://abcdefghij.ws/123a");
+		
+		Assert.assertNotNull(u1);
+		Assert.assertNotNull(u2);
+		
+		Assert.assertThat(u1, is(not(u2)));
+	}
+
+	@Test
+	public void testDifferentPaths() {
+		
+		final Uri u1 = Uri.create("http://abcdefghij.ws/123A");
+		final Uri u2 = Uri.create("http://abcdefghij.ws/____");
+		
+		Assert.assertNotNull(u1);
+		Assert.assertNotNull(u2);
+		
+		Assert.assertThat(u1, is(not(u2)));
+	}
+
+}
