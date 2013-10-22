499c775478a4c027a99a63eccf5a4af40310c546
João Vilaça <jvilaca@gmail.com>
2013-10-22 01:35:35 +0100

    FIX: adapted tests to changes in HashKey class

--- src/test/java/pt/go2/keystore/HashKeyTest.java
+++ src/test/java/pt/go2/keystore/HashKeyTest.java
@@ -18,32 +18,32 @@ public class HashKeyTest {
 	public void testCloning() {
 
 		HashKey hk5 = new HashKey();
-		HashKey hk6 = new HashKey(hk5.getBytes());
+		HashKey hk6 = new HashKey(hk5.toString());
 
 		Assert.assertTrue(hk5.equals(hk6));
 
 		HashKey hk9 = new HashKey();
-		HashKey hk10 = new HashKey(hk9.getBytes());
+		HashKey hk10 = new HashKey(hk9.toString());
 
 		Assert.assertTrue(hk9.equals(hk10));
 
 		HashKey hk11 = new HashKey();
-		HashKey hk12 = new HashKey(hk11.getBytes());
+		HashKey hk12 = new HashKey(hk11.toString());
 
 		Assert.assertTrue(hk11.equals(hk12));
 
-		HashKey hk1 = new HashKey("aaaaa0".getBytes());
-		HashKey hk2 = new HashKey(hk1.getBytes());
+		HashKey hk1 = new HashKey("aaaaa0");
+		HashKey hk2 = new HashKey(hk1.toString());
 
 		Assert.assertTrue(hk1.equals(hk2));
 
-		HashKey hk3 = new HashKey("aAbCeF".getBytes());
-		HashKey hk4 = new HashKey(hk3.getBytes());
+		HashKey hk3 = new HashKey("aAbCeF");
+		HashKey hk4 = new HashKey(hk3.toString());
 
 		Assert.assertTrue(hk3.equals(hk4));
 
-		HashKey hk7 = new HashKey("------".getBytes());
-		HashKey hk8 = new HashKey(hk7.getBytes());
+		HashKey hk7 = new HashKey("------");
+		HashKey hk8 = new HashKey(hk7.toString());
 
 		Assert.assertTrue(hk7.equals(hk8));
 
