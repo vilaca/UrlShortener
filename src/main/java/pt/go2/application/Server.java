7a053babc426fc93174ede3039cb5305d4cd2872
João Vilaça <jvilaca@gmail.com>
2013-11-03 13:13:04 +0000

    FIX: moved multiplexer and access_logging to RequestHandler. Handling close() for Backup/Keystore

--- src/main/java/pt/go2/application/Server.java
+++ src/main/java/pt/go2/application/Server.java
@@ -3,24 +3,12 @@ package pt.go2.application;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import pt.go2.fileio.Configuration;
-import pt.go2.fileio.SmartTagParser;
-import pt.go2.keystore.KeyValueStore;
-import pt.go2.pagelets.PageLet;
-import pt.go2.pagelets.RedirectPageLet;
-import pt.go2.pagelets.ShortenerPageLet;
-import pt.go2.pagelets.StaticPageLetBuilder;
 
-import com.sun.net.httpserver.Headers;
-import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 
 class Server {
@@ -31,9 +19,6 @@ class Server {
 
 	static private BufferedWriter accessLog = null;
 
-	static private KeyValueStore ks;
-
-	
 	/**
 	 * Process initial method
 	 */
@@ -47,20 +32,6 @@ class Server {
 
 		logger.trace("Resuming DB from folder: " + config.DATABASE_FOLDER);
 
-		// restore URI/hash mappings data
-
-		ks = KeyValueStore.create(config.DATABASE_FOLDER, config.REDIRECT);
-			
-		// map static pages to URI part
-
-		final Map<String, PageLet> pages;
-		try {
-			pages = generateResourceDecoder();
-		} catch (IOException ex) {
-			logger.fatal("Error reading resources from file." + ex.getMessage());
-			return;
-		}
-			
 		// create listener
 
 		logger.trace("Server now starting....");
@@ -75,24 +46,26 @@ class Server {
 			return;
 		}
 
-		listener.createContext("/", new RequestHandler(ks, pages, config.VERSION, config.ENFORCE_DOMAIN));
-		listener.setExecutor(null);
+		// start access log
 
 		try {
-			
-			// start access log
+			final FileWriter file = new FileWriter(config.ACCESS_LOG, true);
+			accessLog = new BufferedWriter(file);
+		} catch (IOException e) {
+			System.out.println("Access log redirected to console.");
+		}
 
-			try {
-				final FileWriter file = new FileWriter(config.ACCESS_LOG, true);
-				accessLog = new BufferedWriter(file);
-			} catch (IOException e1) {
-				System.out.println("Access log redirected to console.");
-			}
+		// instantiate the almighty RequestHandler
+		
+		try ( final RequestHandler requestHandler = new RequestHandler(config, accessLog);) {
+
+			listener.createContext("/", requestHandler);
+			listener.setExecutor(null);
 
 			// start server
 
 			listener.start();
-			
+
 			System.out.println("Server Running. Press [k] to kill listener.");
 			boolean running = true;
 			do {
@@ -108,6 +81,10 @@ class Server {
 
 			listener.stop(1);
 
+		} catch (IOException ex) {
+			logger.fatal("Error reading resources from file." + ex.getMessage());
+			return;
+
 		} finally {
 			try {
 				if (accessLog != null) {
@@ -117,120 +94,5 @@ class Server {
 			}
 			logger.trace("Server stopped.");
 		}
-	}
-
-	/**
-	 * URL to Pagelet Multiplexer
-	 * 
-	 * @return
-	 * @throws IOException
-	 */
-	private static Map<String, PageLet> generateResourceDecoder()
-			throws IOException {
-
-		final SmartTagParser fr = new SmartTagParser("/");
-		final Map<String, PageLet> pages = new HashMap<>();
-
-		pages.put("/", 
-				new StaticPageLetBuilder()
-						.setContent(fr.read("index.html")).zip().build());
-
-		pages.put("ajax.js",
-				new StaticPageLetBuilder()
-						.setContent(fr.read("ajax.js"))
-						.setMimeType("application/javascript").zip().build());
-
-		pages.put("robots.txt",
-				new StaticPageLetBuilder()
-						.setContent(fr.read("robots.txt"))
-						.setMimeType("text/plain").zip().build());
-
-		pages.put("sitemap.xml",
-				new StaticPageLetBuilder()
-						.setContent(fr.read("map.txt"))
-						.setMimeType("text/xml").zip().build());
-
-		pages.put("screen.css",
-				new StaticPageLetBuilder()
-						.setContent(fr.read("screen.css"))
-						.setMimeType("text/css").zip().build());
-
-		// dynamic pages
-		pages.put("new", new ShortenerPageLet(ks));
-
-		// error pages
-		pages.put("404",
-				new StaticPageLetBuilder()
-						.setContent(fr.read("404.html"))
-						.setResponseCode(404).zip().build());
-
-		// google webmaster tools site verification
-		
-		if ( !config.GOOGLE_VALIDATION.isEmpty())
-		{
-			pages.put(config.GOOGLE_VALIDATION,
-					new StaticPageLetBuilder()
-							.setContent("google-site-verification: " + config.GOOGLE_VALIDATION)
-							.setResponseCode(200).zip().build());
-		}
-		
-		// redirect to domain if a subdomain is being used
-		
-		pages.put("",
-				new RedirectPageLet(301, "http://" + config.ENFORCE_DOMAIN));
-
-		
-		return pages;
-	}
-	
-	/**
-	 * Access log output
-	 * 
-	 * @param params
-	 * @param response
-	 */
-	static void printLogMessage(final HttpExchange params,
-			final HttpResponse response) {
-
-		final StringBuilder sb = new StringBuilder();
-
-		sb.append(params.getRemoteAddress().getAddress().getHostAddress());
-		sb.append(" - - [");
-		sb.append(new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss Z")
-				.format(new Date()));
-		sb.append("] \"");
-		sb.append(params.getRequestMethod());
-		sb.append(" ");
-		sb.append(params.getRequestURI().toString());
-		sb.append(" ");
-		sb.append(params.getProtocol());
-		sb.append(" ");
-		sb.append(response.getHttpErrorCode());
-		sb.append(" ");
-		sb.append(response.getSize());
-		sb.append(" \"");
-
-		final Headers headers = params.getRequestHeaders();
-		final String referer = headers.getFirst("Referer");
-		final String agent = headers.getFirst("User-Agent");
-		
-		sb.append(referer == null ? "-" : referer);
-
-		sb.append("\" \"" + agent + "\"");
-		sb.append(System.getProperty("line.separator"));
-
-		final String output = sb.toString();
-
-		if (accessLog == null) {
-			System.out.print(output);	
-			return;
-		}
-
-		try {
-			accessLog.write(output);
-			accessLog.flush();
-		} catch (IOException e) {
-		}
-		
 	}
 }
