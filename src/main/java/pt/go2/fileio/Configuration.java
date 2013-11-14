0a44b6f7593817d676f74681fbd5b96c64517fac
João Vilaça <jvilaca@gmail.com>
2013-11-14 23:00:03 +0000

    ADD: configuration key: server.cache-hint

--- src/main/java/pt/go2/fileio/Configuration.java
+++ src/main/java/pt/go2/fileio/Configuration.java
@@ -6,6 +6,9 @@ import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.util.Properties;
 
+/**
+ * Process configuration
+ */
 public class Configuration {
 
 	// resource file locations on JAR
@@ -14,49 +17,50 @@ public class Configuration {
 
 	// apache style access log
 	public final String ACCESS_LOG;
-	
+
 	// server listener backlog
 	public final int BACKLOG;
-	
+
 	// hash->uri restore folder
 	public final String DATABASE_FOLDER;
-	
+
 	// site domain
 	public final String ENFORCE_DOMAIN;
-	
+
 	// Google validation for webmaster tools site
-	public final String GOOGLE_VALIDATION;
-	
+	public final String GOOGLE_VERIFICATION;
+
 	// listener host
 	public final InetSocketAddress HOST;
-	
+
 	// phishing urls database
 	public final String PHISHTANK_API_KEY;
-	
+
 	// redirect status code to be used for short Urls
 	public final int REDIRECT;
+
+	// Amount of time static pages should be cached
+	public final int CACHE_HINT;
 	
 	// server version
 	public final String VERSION;
-		
+
 	/**
 	 * Read configuration from file
 	 */
-	public Configuration ()
-	{
+	public Configuration() {
 		// attempt reading properties/configuration from JAR
-		
-		try (InputStream is = Configuration.class.getResourceAsStream("/"+ PROPERTIES);)
-		{
+
+		try (InputStream is = Configuration.class.getResourceAsStream("/"
+				+ PROPERTIES);) {
 			prop.load(is);
 
 		} catch (IOException e) {
 		}
 
 		// attempt reading properties/configuration from basedir
-		
-		try (InputStream is = new FileInputStream(PROPERTIES);)
-		{
+
+		try (InputStream is = new FileInputStream(PROPERTIES);) {
 			prop.load(is);
 
 		} catch (IOException e) {
@@ -65,21 +69,23 @@ public class Configuration {
 		HOST = createInetSocketAddress();
 		REDIRECT = getPropertyAsInt("server.redirect", 301);
 		DATABASE_FOLDER = getResumeFolder();
-		
+
+		CACHE_HINT = getPropertyAsInt("server.cache", 2);
+
 		BACKLOG = getPropertyAsInt("server.backlog", 100);
 		ACCESS_LOG = getProperty("server.accessLog", "access_log");
 		VERSION = getProperty("server.version", "beta");
-		
-		GOOGLE_VALIDATION = getProperty("google-site-verification", "");
-		
+
+		GOOGLE_VERIFICATION = getProperty("google-site-verification", "");
+
 		ENFORCE_DOMAIN = getProperty("enforce-domain", null);
-		
+
 		PHISHTANK_API_KEY = getProperty("phishtank-api-key");
 	}
-	
+
 	/**
 	 * Use this method only for Smart Tag parsing
-	 *  
+	 * 
 	 * @param string
 	 * 
 	 * @return
@@ -89,7 +95,7 @@ public class Configuration {
 	}
 
 	private int getPropertyAsInt(final String key, final int defaultValue) {
-		
+
 		final String value = prop.getProperty(key);
 
 		if (value != null) {
@@ -99,38 +105,37 @@ public class Configuration {
 			} catch (NumberFormatException nfe) {
 			}
 		}
-		
+
 		return defaultValue;
 	}
 
 	private String getProperty(final String key, final String defaultValue) {
-		
+
 		final String value = prop.getProperty(key);
 
 		if (value == null) {
 
 			return defaultValue;
 		}
-		
+
 		return value;
 	}
-	
+
 	private String getResumeFolder() {
 
 		final String resumeFolder = prop.getProperty("database.folder");
 
 		if (resumeFolder == null) {
 			return "";
-		} 
-		
-		if (resumeFolder.endsWith(System.getProperty("file.separator")))
-		{
+		}
+
+		if (resumeFolder.endsWith(System.getProperty("file.separator"))) {
 			return resumeFolder;
 		}
-		
+
 		return resumeFolder + System.getProperty("file.separator");
 	}
-	
+
 	private InetSocketAddress createInetSocketAddress() {
 
 		// both parameters are optional
