cd8ce4f2ed8354c513de782e19f6e46c8755de09
João Vilaça <jvilaca@gmail.com>
2013-12-04 00:54:07 +0000

    ADD: Page annotations

---
+++ src/main/java/pt/go2/annotations/Page.java
@@ -0,0 +1,14 @@
+package pt.go2.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = ElementType.TYPE)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface Page {
+
+	String path();
+	boolean requireLogin();
+}
