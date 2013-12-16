746b7a1f84115160d67b95aee1f218f77fc6e4de
João Vilaça <jvilaca@gmail.com>
2013-12-16 16:17:28 +0000

    FIX: remove unused import

--- src/main/java/pt/go2/application/Server.java
+++ src/main/java/pt/go2/application/Server.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
