b941d60d5541fdc12a390e0c00dd59a185362019
vilaca <jvilaca@gmail.com>
2014-11-20 21:43:40 +0000

    Fixed https://github.com/vilaca/UrlShortener/issues/25

--- src/main/java/pt/go2/fileio/EmbeddedFiles.java
+++ src/main/java/pt/go2/fileio/EmbeddedFiles.java
@@ -10,48 +10,48 @@ import pt.go2.response.GzipResponse;
 
 public class EmbeddedFiles implements FileSystemInterface {
 
-	final private Map<String, AbstractResponse> pages = new HashMap<>();
+	final Map<String, AbstractResponse> pages = new HashMap<>();
 
 	public EmbeddedFiles(Configuration config) throws IOException {
 
 		final byte[] index, ajax, robots, map, css;
 
-		index = SmartTagParser.read(Resources.class
-				.getResourceAsStream("/index.html"));
+		index = SmartTagParser.read(Resources.class.getResourceAsStream("/index.html"));
 
-		ajax = SmartTagParser.read(Resources.class
-				.getResourceAsStream("/ajax.js"));
+		ajax = SmartTagParser.read(Resources.class.getResourceAsStream("/ajax.js"));
 
-		robots = SmartTagParser.read(Resources.class
-				.getResourceAsStream("/robots.txt"));
+		robots = SmartTagParser.read(Resources.class.getResourceAsStream("/robots.txt"));
 
-		map = SmartTagParser.read(Resources.class
-				.getResourceAsStream("/map.txt"));
+		map = SmartTagParser.read(Resources.class.getResourceAsStream("/map.txt"));
 
-		css = SmartTagParser.read(Resources.class
-				.getResourceAsStream("/screen.css"));
+		css = SmartTagParser.read(Resources.class.getResourceAsStream("/screen.css"));
 
-		this.pages.put("/", new GzipResponse(index,
-				AbstractResponse.MIME_TEXT_HTML));
-		this.pages.put("ajax.js", new GzipResponse(ajax,
-				AbstractResponse.MIME_APP_JAVASCRIPT));
+		this.pages.put("/", new GzipResponse(index, AbstractResponse.MIME_TEXT_HTML));
 
-		this.pages.put("robots.txt", new GzipResponse(robots,
-				AbstractResponse.MIME_TEXT_PLAIN));
+		this.pages.put("ajax.js", new GzipResponse(ajax, AbstractResponse.MIME_APP_JAVASCRIPT));
 
-		this.pages.put("sitemap.xml", new GzipResponse(map,
-				AbstractResponse.MIME_TEXT_XML));
+		this.pages.put("robots.txt", new GzipResponse(robots, AbstractResponse.MIME_TEXT_PLAIN));
 
-		this.pages.put("screen.css", new GzipResponse(css,
-				AbstractResponse.MIME_TEXT_CSS));
+		this.pages.put("sitemap.xml", new GzipResponse(map, AbstractResponse.MIME_TEXT_XML));
+
+		this.pages.put("screen.css", new GzipResponse(css, AbstractResponse.MIME_TEXT_CSS));
 
 		if (!config.GOOGLE_VERIFICATION.isEmpty()) {
-			this.pages
-					.put(config.GOOGLE_VERIFICATION,
-							new GzipResponse(
-									("google-site-verification: " + config.GOOGLE_VERIFICATION)
-											.getBytes(),
-									AbstractResponse.MIME_TEXT_PLAIN));
+			this.pages.put(config.GOOGLE_VERIFICATION, new GzipResponse(
+					("google-site-verification: " + config.GOOGLE_VERIFICATION).getBytes(),
+					AbstractResponse.MIME_TEXT_PLAIN));
+		}
+
+		// check if all pages created
+
+		for (String page : this.pages.keySet()) {
+
+			final AbstractResponse response = this.pages.get(page);
+
+			if (response == null) {
+
+				throw new IOException("Failed to load page " + page);
+			}
 		}
 	}
 
@@ -67,5 +67,4 @@ public class EmbeddedFiles implements FileSystemInterface {
 	public AbstractResponse getFile(String filename) {
 		return pages.get(filename);
 	}
-
 }
