07f81a7ca72b57641099bf391165b36db4f3e32b
vilaca <jvilaca@gmail.com>
2014-11-23 01:29:15 +0000

    clean up

--- src/main/java/pt/go2/external/PhishTankDownloader.java
+++ src/main/java/pt/go2/external/PhishTankDownloader.java
@@ -1,9 +1,8 @@
 package pt.go2.external;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,6 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.http.HttpStatus;
 
 import pt.go2.storage.Uri;
 
@@ -21,8 +21,8 @@ public class PhishTankDownloader {
 	// expected entries on API - used to avoid resizing in loop
 
 	private static final int EXPECTED_ENTRIES = 15000;
-	
-	private final String API_URL;
+
+	private final String apiUrl;
 
 	private final PhishLocalCache banned;
 
@@ -34,7 +34,7 @@ public class PhishTankDownloader {
 	 */
 	public PhishTankDownloader(final String apiKey, PhishLocalCache banned) {
 
-		API_URL = "http://data.phishtank.com/data/" + apiKey + "/online-valid.csv";
+		apiUrl = "http://data.phishtank.com/data/" + apiKey + "/online-valid.csv";
 
 		this.banned = banned;
 	}
@@ -50,7 +50,7 @@ public class PhishTankDownloader {
 	 */
 	public boolean download() {
 
-		final Set<Uri> banned = new HashSet<>(EXPECTED_ENTRIES);
+		final Set<Uri> newList = new HashSet<>(EXPECTED_ENTRIES);
 
 		LOGGER.info("Download starting");
 
@@ -59,7 +59,7 @@ public class PhishTankDownloader {
 		ContentResponse response;
 		try {
 			httpClient.start();
-			response = httpClient.GET(API_URL);
+			response = httpClient.GET(apiUrl);
 		} catch (Exception e) {
 			LOGGER.error(e);
 			return false;
@@ -67,14 +67,12 @@ public class PhishTankDownloader {
 
 		final int statusCode = response.getStatus();
 
-		if (statusCode != 200) {
+		if (statusCode != HttpStatus.OK_200) {
 			LOGGER.error("Error on download: " + statusCode);
 			return false;
 		}
 
-		final ByteArrayInputStream ba = new ByteArrayInputStream(response.getContent());
-
-		final BufferedReader br = new BufferedReader(new InputStreamReader(ba));
+		final BufferedReader br = new BufferedReader(new StringReader(response.getContentAsString()));
 
 		try {
 
@@ -101,19 +99,20 @@ public class PhishTankDownloader {
 				final Uri uri;
 				uri = Uri.create(entry.substring(idx, end), false);
 
-				banned.add(uri);
+				newList.add(uri);
 			}
 		} catch (IOException e) {
 			LOGGER.error(e);
 			return false;
 		} finally {
 			try {
-				ba.close();
+				br.close();
 			} catch (IOException e) {
+				LOGGER.error(e);
 			}
 		}
 
-		this.banned.set(banned);
+		this.banned.set(newList);
 
 		LOGGER.info("Download exiting");
 
