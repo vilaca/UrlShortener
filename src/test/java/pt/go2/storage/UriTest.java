b0ed053e6f6340795b406a53a775ef52b123d20f
vilaca <jvilaca@gmail.com>
2014-11-27 01:34:03 +0000

    Fixed tests

--- src/test/java/pt/go2/storage/UriTest.java
+++ src/test/java/pt/go2/storage/UriTest.java
@@ -1,108 +1,107 @@
-
 package pt.go2.storage;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+
+import java.io.UnsupportedEncodingException;
 
 import org.junit.Assert;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.*;
 
 /**
  * Test Uri class, don't mind testing URL validator
- * 
- * @author vilaca
- *
  */
 public class UriTest {
 
-	@Test
-	public void testDomain() {
-		
-		final Uri u1 = Uri.create("http://abcdefghij.ws/123", false);
-		final Uri u2 = Uri.create("http://www.abcdefghij.ws/123", false);
-		final Uri u3 = Uri.create("http://www.abcdefghij.ws:80/123", false);
+    @Test
+    public void testDomain() throws UnsupportedEncodingException {
 
-		Assert.assertEquals("abcdefghij.ws",u1.domain());
-		Assert.assertEquals("abcdefghij.ws",u2.domain());
-		Assert.assertEquals("abcdefghij.ws",u3.domain());
-	}
+        final Uri u1 = Uri.create("http://abcdefghij.ws/123", false);
+        final Uri u2 = Uri.create("http://www.abcdefghij.ws/123", false);
+        final Uri u3 = Uri.create("http://www.abcdefghij.ws:80/123", false);
 
-	@Test
-	public void testEqual() {
-		
-		final Uri u1 = Uri.create("http://abcdefghij.ws/123", false);
-		final Uri u2 = Uri.create("http://ABCDEFGHIJ.ws/123", false);
-		final Uri u3 = Uri.create("http://ABCDEFGHIJ.WS/123", false);
+        Assert.assertEquals("abcdefghij.ws", u1.domain());
+        Assert.assertEquals("abcdefghij.ws", u2.domain());
+        Assert.assertEquals("abcdefghij.ws", u3.domain());
+    }
 
-		Assert.assertNotNull(u1);
-		Assert.assertNotNull(u2);
-		Assert.assertNotNull(u3);		
-		
-		Assert.assertThat(u1, is(u2));
-		Assert.assertThat(u2, is(u3));
-		Assert.assertThat(u3, is(u1));
-	}
+    @Test
+    public void testEqual() {
 
+        final Uri u1 = Uri.create("http://abcdefghij.ws/123", false);
+        final Uri u2 = Uri.create("http://ABCDEFGHIJ.ws/123", false);
+        final Uri u3 = Uri.create("http://ABCDEFGHIJ.WS/123", false);
 
-	@Test
-	public void testEqualHttp() {
-		
-		final Uri u1 = Uri.create("http://abcdefghij.ws/123", false);
-		final Uri u2 = Uri.create("ABCDEFGHIJ.ws/123", false);
+        Assert.assertNotNull(u1);
+        Assert.assertNotNull(u2);
+        Assert.assertNotNull(u3);
 
-		Assert.assertNotNull(u1);
-		Assert.assertNotNull(u2);
-		
-		Assert.assertThat(u1, is(u2));
-	}
+        Assert.assertThat(u1, is(u2));
+        Assert.assertThat(u2, is(u3));
+        Assert.assertThat(u3, is(u1));
+    }
 
-	@Test
-	public void testTrim() {
-		
-		final Uri u1 = Uri.create(" http://abcdefghij.ws/123", false);
-		final Uri u2 = Uri.create("http://abcdefghij.ws/123 ", false);
-		final Uri u3 = Uri.create(" http://abcdefghij.ws/123 ", false);
-		
-		Assert.assertNotNull(u1);
-		Assert.assertNotNull(u2);
-		Assert.assertNotNull(u3);
-		
-		Assert.assertThat(u1, is(u2));
-		Assert.assertThat(u2, is(u3));
-		Assert.assertThat(u3, is(u1));
-	}
+    @Test
+    public void testEqualHttp() {
 
-	@Test
-	public void testDifferentScheme() {
-		
-		final Uri u1 = Uri.create("http://abcdefghij.ws/123", false);
-		final Uri u2 = Uri.create("https://ABCDEFGHIJ.ws/123", false);
-		
-		Assert.assertNotNull(u1);
-		Assert.assertNotNull(u2);
-		
-		Assert.assertThat(u1, is(not(u2)));
-	}
+        final Uri u1 = Uri.create("http://abcdefghij.ws/123", false);
+        final Uri u2 = Uri.create("ABCDEFGHIJ.ws/123", false);
 
-	@Test
-	public void testDifferentCaseInPath() {
-		
-		final Uri u1 = Uri.create("http://abcdefghij.ws/123A", false);
-		final Uri u2 = Uri.create("http://abcdefghij.ws/123a", false);
-		
-		Assert.assertNotNull(u1);
-		Assert.assertNotNull(u2);
-		
-		Assert.assertThat(u1, is(not(u2)));
-	}
+        Assert.assertNotNull(u1);
+        Assert.assertNotNull(u2);
 
-	@Test
-	public void testDifferentPaths() {
-		
-		final Uri u1 = Uri.create("http://abcdefghij.ws/123A", false);
-		final Uri u2 = Uri.create("http://abcdefghij.ws/____", false);
-		
-		Assert.assertNotNull(u1);
-		Assert.assertNotNull(u2);
-		
-		Assert.assertThat(u1, is(not(u2)));
-	}
+        Assert.assertThat(u1, is(u2));
+    }
+
+    @Test
+    public void testTrim() {
+
+        final Uri u1 = Uri.create(" http://abcdefghij.ws/123", false);
+        final Uri u2 = Uri.create("http://abcdefghij.ws/123 ", false);
+        final Uri u3 = Uri.create(" http://abcdefghij.ws/123 ", false);
+
+        Assert.assertNotNull(u1);
+        Assert.assertNotNull(u2);
+        Assert.assertNotNull(u3);
+
+        Assert.assertThat(u1, is(u2));
+        Assert.assertThat(u2, is(u3));
+        Assert.assertThat(u3, is(u1));
+    }
+
+    @Test
+    public void testDifferentScheme() {
+
+        final Uri u1 = Uri.create("http://abcdefghij.ws/123", false);
+        final Uri u2 = Uri.create("https://ABCDEFGHIJ.ws/123", false);
+
+        Assert.assertNotNull(u1);
+        Assert.assertNotNull(u2);
+
+        Assert.assertThat(u1, is(not(u2)));
+    }
+
+    @Test
+    public void testDifferentCaseInPath() {
+
+        final Uri u1 = Uri.create("http://abcdefghij.ws/123A", false);
+        final Uri u2 = Uri.create("http://abcdefghij.ws/123a", false);
+
+        Assert.assertNotNull(u1);
+        Assert.assertNotNull(u2);
+
+        Assert.assertThat(u1, is(not(u2)));
+    }
+
+    @Test
+    public void testDifferentPaths() {
+
+        final Uri u1 = Uri.create("http://abcdefghij.ws/123A", false);
+        final Uri u2 = Uri.create("http://abcdefghij.ws/____", false);
+
+        Assert.assertNotNull(u1);
+        Assert.assertNotNull(u2);
+
+        Assert.assertThat(u1, is(not(u2)));
+    }
 }
