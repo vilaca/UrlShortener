4a44f51edc63b4c0abf420e66c5e240cae359481
vilaca <jvilaca@gmail.com>
2014-11-27 00:56:51 +0000

    Fixed HashKey tests

--- src/test/java/pt/go2/storage/HashKeyTest.java
+++ src/test/java/pt/go2/storage/HashKeyTest.java
@@ -1,51 +1,28 @@
-/**
- * 
- */
 package pt.go2.storage;
+
+import java.io.UnsupportedEncodingException;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-
-/**
- * 
- * @author vilaca
- * 
- */
 public class HashKeyTest {
 
-	@Test
-	public void testCloning() {
+    @Test
+    public void testEquals() throws UnsupportedEncodingException {
 
-		HashKey hk5 = new HashKey();
-		HashKey hk6 = new HashKey(hk5.toString());
+        final HashKey hk5 = new HashKey("AABBCC");
+        final HashKey hk6 = new HashKey("AABBCC");
 
-		Assert.assertTrue(hk5.equals(hk6));
+        Assert.assertTrue(hk5.equals(hk6));
+    }
 
-		HashKey hk9 = new HashKey();
-		HashKey hk10 = new HashKey(hk9.toString());
+    @Test
+    public void testDecoding() throws UnsupportedEncodingException {
 
-		Assert.assertTrue(hk9.equals(hk10));
+        final HashKey hk5 = new HashKey("AABBCC");
+        final HashKey hk6 = new HashKey(hk5.toString());
 
-		HashKey hk11 = new HashKey();
-		HashKey hk12 = new HashKey(hk11.toString());
+        Assert.assertTrue(hk5.equals(hk6));
 
-		Assert.assertTrue(hk11.equals(hk12));
-
-		HashKey hk1 = new HashKey("aaaaa0");
-		HashKey hk2 = new HashKey(hk1.toString());
-
-		Assert.assertTrue(hk1.equals(hk2));
-
-		HashKey hk3 = new HashKey("aAbCeF");
-		HashKey hk4 = new HashKey(hk3.toString());
-
-		Assert.assertTrue(hk3.equals(hk4));
-
-		HashKey hk7 = new HashKey("------");
-		HashKey hk8 = new HashKey(hk7.toString());
-
-		Assert.assertTrue(hk7.equals(hk8));
-
-	}
+    }
 }
