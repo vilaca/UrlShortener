2d8777eb4a75925d4a863f085c263f1efb86dc14
vilaca <jvilaca@gmail.com>
2016-04-15 20:35:03 +0100

    clean up - unused methods

--- src/main/java/pt/go2/response/GenericResponse.java
+++ src/main/java/pt/go2/response/GenericResponse.java
@@ -1,8 +1,6 @@
 package pt.go2.response;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,39 +8,13 @@ import org.eclipse.jetty.http.HttpStatus;
 
 public class GenericResponse implements Response {
 
-    private static final int BUFFER_SIZE = 4096;
-
     final byte[] body;
     final int error;
     final String mime;
 
-    public static GenericResponse createFromFile(InputStream is, int error) throws IOException {
-
-        final byte[] buffer = new byte[BUFFER_SIZE];
-        int read;
-
-        final ByteArrayOutputStream output = new ByteArrayOutputStream();
-
-        while ((read = is.read(buffer)) != -1) {
-            output.write(buffer, 0, read);
-        }
-
-        return new GenericResponse(output.toByteArray(), error, MIME_TEXT_HTML);
-    }
-
     public static GenericResponse create(byte[] body, String mime) {
 
         return new GenericResponse(body, HttpStatus.OK_200, mime);
-    }
-
-    public static GenericResponse NotOk(int status) {
-
-        return new GenericResponse(new byte[0], status, MIME_TEXT_HTML);
-    }
-
-    public static GenericResponse createForbidden(byte[] body) {
-
-        return new GenericResponse(body, HttpStatus.FORBIDDEN_403, MIME_TEXT_HTML);
     }
 
     private GenericResponse(byte[] body, int error, String mime) {
@@ -69,19 +41,15 @@ public class GenericResponse implements Response {
 
     @Override
     public void run(HttpServletResponse exchange) throws IOException {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public boolean isZipped() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean isCacheable() {
-        // TODO Auto-generated method stub
         return false;
     }
 }
