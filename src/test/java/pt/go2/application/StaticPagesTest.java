b5ed06c3b05a053f67fccc9944fcfffc06e6f004
vilaca <jvilaca@gmail.com>
2016-04-27 00:04:43 +0100

    Embedded pages test

--- src/test/java/pt/go2/application/StaticPagesTest.java
+++ src/test/java/pt/go2/application/StaticPagesTest.java
@@ -2,52 +2,51 @@ package pt.go2.application;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.servlet.ServletException;
 
-import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.junit.Assert;
 import org.junit.Test;
 
-import pt.go2.fileio.Configuration;
-import pt.go2.fileio.RestoreItem;
-import pt.go2.mocks.HttpServletRequestMock;
-import pt.go2.mocks.HttpServletResponseMock;
-import pt.go2.storage.KeyValueStore;
+import pt.go2.application.EmbeddedPages.Builder;
 
 public class StaticPagesTest {
 
     @Test
-    public void test() throws IOException, ServletException, URISyntaxException {
+    public void testEmpty() throws IOException, ServletException, URISyntaxException {
 
-        final String redirected = "http://redirected.com";
+        final Builder builder = new Builder(); 
+        final EmbeddedPages ep = builder.create(); 
+        
+        Assert.assertNull(ep.getFile("wtv"));
+    }
 
-        final List<RestoreItem> uris = Arrays.asList(new RestoreItem("aabbcc", redirected));
+    @Test
+    public void testSimpleFileBuilder() throws IOException, ServletException, URISyntaxException {
 
-        final Configuration config = new Configuration();
+        final Builder builder = new Builder(); 
+        
+        builder.add("filename", "content".getBytes(), MimeTypeConstants.MIME_TEXT_PLAIN);
 
-        final KeyValueStore ks = new KeyValueStore(uris, config.getDbFolder());
-        final EmbeddedPages res = new EmbeddedPages(config);
+        final EmbeddedPages ep = builder.create(); 
+        
+        Assert.assertNull(ep.getFile("wtv"));
+        Assert.assertNotNull(ep.getFile("filename"));
+    }
 
-        final HttpServletRequestMock request = new HttpServletRequestMock() {
+    @Test
+    public void testAlias() throws IOException, ServletException, URISyntaxException {
 
-            @Override
-            public String getRequestURI() {
-                return "/aabbcc";
-            }
+        final Builder builder = new Builder(); 
+        
+        builder.add("filename", "content".getBytes(), MimeTypeConstants.MIME_TEXT_PLAIN);
 
-        };
-
-        final HttpServletResponseMock response = new HttpServletResponseMock();
-
-        final AbstractHandler sp = new RequestHandler(config, ks, res, null);
-
-        sp.handle("", null, request, response);
-
-        Assert.assertEquals(config.getRedirect(), response.getStatus());
-        Assert.assertEquals(redirected, response.getHeader(HeaderConstants.RESPONSE_HEADER_LOCATION.toString()));
-
+        builder.setAlias("alias", "filename");
+        
+        final EmbeddedPages ep = builder.create(); 
+        
+        Assert.assertNull(ep.getFile("wtv"));
+        Assert.assertNotNull(ep.getFile("filename"));
+        Assert.assertNotNull(ep.getFile("alias"));
     }
 }
