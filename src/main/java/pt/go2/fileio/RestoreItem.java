3d16345d60f3d552d68e009d7901ccbe2681c6d0
vilaca <jvilaca@gmail.com>
2016-05-09 02:26:26 +0100

    changed encoding to utf-8

--- src/main/java/pt/go2/fileio/RestoreItem.java
+++ src/main/java/pt/go2/fileio/RestoreItem.java
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2016 Jo„o VilaÁa
+    Copyright (C) 2016 Jo√£o Vila√ßa
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
